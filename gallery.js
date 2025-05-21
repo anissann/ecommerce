f47ea7608e8c93e3912fdb30bb5fc6066286463a
Anissa <anisa.abdramanova@mail.ru>
2025-05-22 04:30:47 +0500

    Обновление: изменения в gallery.js, index.html, main.css и main.js

--- gallery.js
+++ gallery.js
@@ -1,64 +1,65 @@
-const mainImages = document.querySelectorAll('.default .main-img img');
-const thumbnails = document.querySelectorAll('.default .thumb-list div');
-const lightboxMainImages = document.querySelectorAll('.lightbox .main-img img');
-const lightboxThumbnails = document.querySelectorAll('.lightbox .thumb-list div')
-const lightbox = document.querySelector('.lightbox')
-const iconClose = document.querySelector('.icon-close')
-const iconPrev = document.querySelector('.icon-prev')
-const iconNext = document.querySelector('.icon-next')
 
-let currentIndex = 0;
+const mainImages = document.querySelectorAll(".default .main-img img");
+const thumbnails = document.querySelectorAll(".default .thumb-list div");
+const lightboxMainImages = document.querySelectorAll(".lightbox .main-img img");
+const lightboxThumbnails = document.querySelectorAll(
+    ".lightbox .thumb-list div"
+);
+const lightbox = document.querySelector(".lightbox");
+const iconClose = document.querySelector(".icon-close");
+const iconPrev = document.querySelector(".icon-prev");
+const iconNext = document.querySelector(".icon-next");
+
+let currentImageIndex = 0;
 
 const changeImage = (index, mainImages, thumbnails) => {
     mainImages.forEach((img) => {
-        img.classList.remove('active')
+        img.classList.remove("active");
     });
-
     thumbnails.forEach((thumb) => {
-        thumb.classList.remove('active')
+        thumb.classList.remove("active");
     });
 
-    mainImages[index].classList.add('active')
-    thumbnails[index].classList.add('active')
-
-    currentIndex = index
-}
+    mainImages[index].classList.add("active");
+    thumbnails[index].classList.add("active");
+    currentImageIndex = index;
+};
 
 thumbnails.forEach((thumb, index) => {
-    thumb.addEventListener('click', () =>{
+    thumb.addEventListener("click", () => {
         changeImage(index, mainImages, thumbnails);
-    })
-})
+    });
+});
 
 lightboxThumbnails.forEach((thumb, index) => {
-    thumb.addEventListener('click', () =>{
+    thumb.addEventListener("click", () => {
         changeImage(index, lightboxMainImages, lightboxThumbnails);
-    })
-})
+    });
+});
 
-mainImages.forEach((img, index)=>{
-    img.addEventListener('click', () => {
-        lightbox.classList.add('active')
+mainImages.forEach((img, index) => {
+    img.addEventListener("click", () => {
+        lightbox.classList.add("active");
         changeImage(index, lightboxMainImages, lightboxThumbnails);
-    })
-})
+    });
+});
 
-iconPrev.addEventListener('click', () => {
-    if(currentIndex <= 0) {
-        changeImage(mainImages.length - 1, lightboxMainImages, lightboxThumbnails)
+iconPrev.addEventListener("click", () => {
+    if (currentImageIndex <= 0) {
+        changeImage(mainImages.length - 1, lightboxMainImages, lightboxThumbnails);
     } else {
-        changeImage(currentIndex - 1, lightboxMainImages, lightboxThumbnails)
+        changeImage(currentImageIndex - 1, lightboxMainImages, lightboxThumbnails);
     }
-})
+});
 
-iconNext.addEventListener('click', () => {
-    if(currentIndex >= mainImages.length - 1) {
-        changeImage(0, lightboxMainImages, lightboxThumbnails)
+iconNext.addEventListener("click", () => {
+    if (currentImageIndex >= mainImages.length - 1) {
+        changeImage(0, lightboxMainImages, lightboxThumbnails);
     } else {
-        changeImage(currentIndex + 1, lightboxMainImages, lightboxThumbnails)
+        changeImage(currentImageIndex + 1, lightboxMainImages, lightboxThumbnails);
     }
-})
+});
 
-iconClose.addEventListener('click', ()=>{
-    lightbox.classList.remove('active');
-})
+iconClose.addEventListener("click", () => {
+    lightbox.classList.remove("active");
+});
--- main.js
+++ main.js
@@ -1,17 +1,20 @@
-const menuIcon = document.querySelector('.menu-icon');
-const backdrop = document.querySelector('.backdrop');
-const navLinks = document.querySelector('.nav-links');
-const closeIcon = document.querySelector('.close-icon');
 
-menuIcon.addEventListener('click', () =>{
-    navLinks.classList.add('active');
-    backdrop.classList.add('active');
+const menuIcon = document.querySelector('.menu-icon')
+const backdrop = document.querySelector('.backdrop')
+const navLinks = document.querySelector('.nav-links')
+const closeIcon = document.querySelector('.close-icon')
+
+menuIcon.addEventListener('click', () => {
+    backdrop.classList.add('active')
+    navLinks.classList.add('active')
 })
-closeIcon.addEventListener('click', () =>{
-    navLinks.classList.remove('active');
-    backdrop.classList.remove('active');
+
+backdrop.addEventListener('click', () => {
+    backdrop.classList.remove('active')
+    navLinks.classList.remove('active')
 })
-backdrop.addEventListener('click', () =>{
-    navLinks.classList.remove('active');
-    backdrop.classList.remove('active');
+
+closeIcon.addEventListener('click', () => {
+    backdrop.classList.remove('active')
+    navLinks.classList.remove('active')
 })
